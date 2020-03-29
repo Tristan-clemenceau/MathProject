fb7edf77cf28b00da8f61eebf96da0ded7a4a003
Tristan-clemenceau <tristan.clemenceau229@gmail.com>
2020-03-29 22:25:29 +0200

    Ajout constructeur

--- ProjetMaths/src/Classes/AlgoNaif.java
+++ ProjetMaths/src/Classes/AlgoNaif.java
@@ -1,5 +1,19 @@
 package Classes;
 
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author trist
+ *
+ */
 public class AlgoNaif {
-	String yo = "yo";
+	final static List<Integer> primaryNumber = new ArrayList<Integer>();
+
+	public AlgoNaif() {
+		
+		// TODO Auto-generated constructor stub
+	}
+	
+	
 }
