48db0f1aee8bf35bb7b6065e71022600b549ac1f
Tristan-clemenceau <tristan.clemenceau229@gmail.com>
2020-05-02 17:13:16 +0200

    Adding indent

--- ProjetMaths/src/Classes/Main.java
+++ ProjetMaths/src/Classes/Main.java
@@ -5,7 +5,9 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		long n = 53628957L;
+		
 		AlgoNaif algo1 = new AlgoNaif();
+		
 		algo1.factoristation(n);
 
 	}
